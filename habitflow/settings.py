f304849c8ebfc698925e6911d0646cb9556ddc97
Стефания Пикулева <sdpikuleva@edu.hse.ru>
2025-03-27 00:56:13 +0300

    connecting the database postgresql

--- habitflow/settings.py
+++ habitflow/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'habitflow.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'habitflow_db',
+        'USER': 'habituser',
+        'PASSWORD': 'habitflow',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
