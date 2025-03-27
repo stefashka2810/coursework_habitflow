bd65b5b959d8577373382f3c984aa4fc7ba5f807
Стефания Пикулева <sdpikuleva@edu.hse.ru>
2025-03-27 16:09:18 +0300

    create the form of registration

---
+++ users/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
+from .models import User
+
+class UserRegisterForm(UserCreationForm):
+    
+    password1 = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput(attrs={'class': 'form-control'}),
+        help_text="Your password must contain at least 8 characters"
+    )
+
+    password2 = forms.CharField(
+        label="Password confirmation",
+        widget=forms.PasswordInput(attrs={'class': 'form-control'}),
+        help_text="Enter the same password as before, for verification"
+    )
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'age', 'gender', 'email', 'password1', 'password2']
+        
+    def clean_password2(self):
+
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("Passwords don't match")
+        return password2
